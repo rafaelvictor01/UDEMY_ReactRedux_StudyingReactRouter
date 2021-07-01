7ab0f3d21793d3c3dc07affc2cdb76140fa2d85d
Rafael Victor <rafaelvictor.bernardes@gmail.com>
2021-07-01 17:44:30 -0300

    Estrutura mínima do projeto. Criação do componente APP. Remoção de lixo

---
+++ src/app/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import './App.css'
+
+/**
+ * Componente com maior grau de abstração da aplicação.
+ * @author rafaelvictor01
+ * @returns JSX.Element
+ */
+export default function App() {
+    return (
+        <div className={'App'}>
+            Componente APP
+        </div>
+    )
+}
